55403b79249bcd29f0c026bd631caf0822bf5bef
Yuhyun <yuhyun934@gmail.com>
2026-01-13 22:15:22 -0600

    feat-2: add character movement script and attach camera to character

--- untitled-godot/frogger start files/scenes/player.gd
+++ untitled-godot/frogger start files/scenes/player.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
+var direction: Vector2 = Vector2(1,1)
+var speed: int = 5
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _physics_process(delta: float) -> void:
+	direction = Input.get_vector("left", "right", "up", "down")
+	position += direction * speed
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+  
+	 
