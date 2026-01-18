2c8ff0e7419495342eb886c6434ff335f0ee808c
Yuhyun <yuhyun934@gmail.com>
2026-01-17 19:14:05 -0600

    feat-5: add title screen and return player to title screen when they die

--- untitled-godot/frogger start files/scenes/game.gd
+++ untitled-godot/frogger start files/scenes/game.gd
@@ -2,8 +2,23 @@ extends Node2D
 
 var car_scene: PackedScene = preload("res://scenes/car_scene.tscn")
 
+var time_elapsed = 0
+
+func _process(delta):
+	time_elapsed += delta
+	#var minutes = int(time_elapsed / 60)
+	#var seconds = int(time_elapsed % 60)
+	#var time_str = "%02d:%02d" % [minutes, seconds]
+	$CanvasLayer/Label.text = $CanvasLayer/Label.text.get_slice(' ',0) + " " + str(int(time_elapsed))
+	
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	print('character entered the area')
+	call_deferred("change_scene")
+	print(body)
+
+
+func change_scene():
+	get_tree().change_scene_to_file("res://scenes/title_screen.tscn")
 
 
 func _on_timer_timeout() -> void:
@@ -14,5 +29,4 @@ func _on_timer_timeout() -> void:
 	car.get_child(0).connect("body_entered", go_to_title)
 
 func go_to_title(body):
-	print(body)
-	print('player died')	
+	call_deferred("change_scene")
